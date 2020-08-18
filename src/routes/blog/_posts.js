6987f3e2594344ea819274c0f32e9c79b276196d
Reza Handzalah <rezahandzalah@gmail.com>
2020-08-18 09:06:13 +0700

    add new post

--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -1,11 +1,12 @@
 // TODO: if after many posts this becomes unmaintainable:
 // https://github.com/Charca/sapper-blog-template/blob/master/src/routes/blog/_posts.js
 
+import { post3 } from "./stories/Ramble1";
 import { post2 } from "./stories/Gojek_Clone";
 import { post1 } from "./stories/New_Blog";
 
 // fs to read all in folder
-let posts = [post2, post1];
+let posts = [post3, post2, post1];
 
 posts.forEach((post) => {
   post.html = post.html.replace(/^\t{3}/gm, "");
---
+++ src/routes/blog/stories/Ramble1.js
@@ -0,0 +1,109 @@
+const html = `
+<style>
+  h3 { margin-top: 2em; };
+  </style>
+
+<p>
+    Human state of mind is complex, and, by the reason of that, our interactions are as complex as
+    our minds. One of the proofs is when we offer to help someone. Helping is universally accepted
+    as a nice gesture, however, when we genuinely want to help, one can think that we are probably
+    up to something—there’s a hidden intention, or other whole lot of possibilities by connecting our
+    kind gesture to other possibilities.
+</p>
+<p>
+    This actually doesn&#39;t have a context as concrete as a recent incident that I saw or experience.
+    However, there will be the time when it&#39;s possibly happened, and maybe someday I will need to
+    soothe myself from feeling useless when my effort isn&#39;t appreciated, or I probably will be the one
+    who is taking offense. I hope having this thought in written form priorly will ease my brain when
+    that time eventually comes. It can happen anywhere from open source to office environment,
+    household to local community.
+</p>
+
+<h3>Why some people take offense in everything</h3>
+<p>
+    People tend to connect dots; when something happen, we connect it with thousands of other
+    possibilities. We connect those dots not because it&#39;s logical to do so, but it’s because our minds
+    are used to wander that way. It’s in our nature.
+</p>
+<p>
+    Human interaction is complex; it consists with many dots that we can connect. Other than that,
+    messages that are relevant to parties involved may not always be spontaneous or unintended.
+    Some are deliberate, some just cloud in the sky that looks like a rabbit, faces in object. These
+    unlock many other dots that one can possibly connect. Nevertheless, observing too deep into
+    things and connecting those dots causes things might look or even get bigger than it was.
+</p>
+<p>
+    From their point of view, when we offer our help, connecting the dots may come in thinking that
+    we are insincere in helping them—like we have hidden intention other than genuinely wants to
+    offer a helping hand. To begin with, here are some hypothetical dots:
+
+    <ul>
+        <li>Your teammate has something that will benefit the team.</li>
+        <li>You have an idea to improve that thing. As a decent teammate, you ask for their permission to realize your idea.</li>
+        <li>You have been in the company for 4 years.</li>
+    </ul>
+
+    Those hypothetical dots might lead to another conclusion to be drawn to:
+    <ul>
+        <li>You are only doing this because you want to get a promotion.</li>
+    </ul>
+
+    From the conclusion above, despite your genuine intention to improve the team, they think you
+    offer to realize your idea just for the sake of a promotion. Here are more dots:
+    <ul>
+        <li>Your teammate agrees to listen to your idea, then you explain how doing X will improve things</li>
+        <li>Your teammate sees some flaws in the details and asks you about it</li>
+    </ul>
+
+    From your point of view, when your teammate questions your idea, you may assume that they
+    are dishonest. You try to elaborate your idea, but are not even really convinced because after all,
+    it’s just an idea. You conclude that they only question about your idea just in spite of seeing you fail.
+</p>
+
+<h3>Why refrain from helping</h3>
+<p>
+    Legacy is something that is handed down by someone. The thing isn&#39;t necessarily ground-
+    breaking or hard to make, it&#39;s just associated with a person. Not everyone leaves legacy too,
+    which kind of makes it quite an achievement to leave a legacy.
+</p>
+<p>
+    When we offer to help someone, we also give away such thing as our ideas. If we decide to
+    release that idea only for ourselves, the credit goes a hundred percent to us—we are the one that
+    leaves legacy. On the other hand, when we offer to help, we share a credit or even there might be
+    the case where we don’t get credit at all for something that initially come from us. Credit sharing
+    devalues our legacy.
+</p>
+
+<h3>Offer, but don&#39;t be disappointed</h3>
+<p>
+    There might be the time when we can’t resist to offer our help. Or maybe, some of us is simply
+    that kind of people who don’t have the heart to see someone is struggling. At this point, we can’t
+    help but to offer help.
+</p>
+<p>
+    When we find ourselves caught up in that moment—when we can’t resist to offer help, we
+    should be ready for the possible outcome—that maybe the one who we offered to help connect
+    the dots and draw their own assumption regarding our supposedly kind gesture.
+</p>
+<p>
+    Although we have pure, genuine intention to help, there’s not a thing can stop that kind of
+    assumption thrown at us—it’s inevitable. When this happen, we need to soothe ourselves that it’s
+    okay if people who doesn’t know any better throw their own judgement on us, we just need to
+    work our way through the uneasy feeling that it cost us. Don’t let them ruin our good intention.
+    And it’s even better if we can seek middle ground when possible.
+</p>
+<p>
+    &quot;Why?&quot; you ask. The person proceeds to explain his reasoning. It&#39;s correct, but for some reason
+    you can tell it&#39;s the real reason. You refute the point they were trying to make. At this point, you
+    know a ramble will come next. You decide to just nod and step away. This debate is not worth it.
+</p>
+<p>
+    At the end of the day, you are still giving value. It&#39;s just a thank-you that you are missing.
+</p>
+`;
+
+export const post3 = {
+  title: "Offend by Helping",
+  slug: "2020-08-18-offend-by-helping",
+  html: html,
+};
